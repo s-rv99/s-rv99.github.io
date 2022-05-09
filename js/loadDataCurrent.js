2327e28c8f3f9e0f3afc9c1345532709bd0d8ea1
s-rv99 <95875709+s-rv99@users.noreply.github.com>
2022-05-09 23:21:01 +0200

    Something

---
+++ js/loadDataCurrent.js
@@ -0,0 +1,7 @@
+async function getBalance(){
+    result = await fetch("https://api.spyglass.pw/banano/v1/account/overview/ban_1ricex9kie3if1qros9s84joa6hnqd97nqc9fo58ca4ro3b1hnpyujjixoyt",{method:"GET"});
+    jsonObj = await result.json();
+    document.getElementById('balance').innerText = jsonObj.balance.toFixed(2);
+    console.log(jsonObj);
+}
+getBalance();
