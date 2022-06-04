96c877ef9679eb02bb0b784393a21d4010ef1f42
s-rv99 <95875709+s-rv99@users.noreply.github.com>
2022-06-04 23:58:27 +0200

    Total in leaderboard

--- js/loadData.js
+++ js/loadData.js
@@ -1,6 +1,7 @@
 async function loadData(){
 	pages = 1;
 	table = document.getElementById('b');
+	footer = document.getElementById('f');
 	r = await fetch("https://raw.githubusercontent.com/s-rv99/s-rv99.github.io/main/json/assoc.json",{method:"GET"});
 	assoc = await r.json();
 	document.getElementById('l1').innerText = "✔";
@@ -9,6 +10,7 @@ async function loadData(){
 	console.log(jsonCheat);
 	document.getElementById('l2').innerText = "✔";
 	c = 0;
+	total = 0;
 	for(let p = 1; p <= pages; p++){
 		r = await fetch("https://engine.freerice.com/group-members?_format=json&group=57a09e6b-505f-4192-95d5-d214e139fe9f&current=" + p + "&limit=50",{method:"GET"});
 		jsonObj = await r.json();
@@ -26,9 +28,15 @@ async function loadData(){
 					console.log(jsonObj.data[i].attributes.user);
 				}
   				cell3.innerText = jsonObj.data[i].attributes.rice;
+				total += jsonObj.data[i].attributes.rice;
 			}
 		}
 	}
+	row = footer.insertRow(0);
+	cell1 = row.insertCell(0);
+  	cell2 = row.insertCell(1);
+  	cell3 = row.insertCell(2);
+	cell3.innerText = total;
 	document.getElementById('load').classList.add('hide');
 }
 loadData();
