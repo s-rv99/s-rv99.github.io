521cd66c9394d6f147e8cd7ef6125252c221dd38
s-rv99 <95875709+s-rv99@users.noreply.github.com>
2022-06-05 00:12:16 +0200

    Update createSnapshot.js

--- js/createSnapshot.js
+++ js/createSnapshot.js
@@ -4,21 +4,21 @@ async function loadData(){
 	assoc = await r.json();
 	document.getElementById('l1').innerText = "✔";
 	c = 1;
-	text = "{\"time\":\"" + Date.now() + "\",\"data\":[";
+	text = "{\"time\":\"" + Date.now() + "\",\"data\":{";
 	for(let p = 1; p <= pages; p++){
 		r = await fetch("https://engine.freerice.com/group-members?_format=json&group=57a09e6b-505f-4192-95d5-d214e139fe9f&current=" + p + "&limit=50",{method:"GET"});
 		jsonObj = await r.json();
 		pages = jsonObj.meta.pagination.total_pages;
 		document.getElementById('l2').innerText = p + "/" + pages;
 		for(let i = 0; i < jsonObj.meta.pagination.count; i++){
-			text += "{\"rank\":\"" + c + "\",\"username\":\"" + assoc[jsonObj.data[i].attributes.user] + "\",\"userid\":\"" + jsonObj.data[i].attributes.user + "\",\"rice\":\"" + jsonObj.data[i].attributes.rice + "\"}";
+			text += "\"" + jsonObj.data[i].attributes.user + "\":{\"rank\":\"" + c + "\",\"username\":\"" + assoc[jsonObj.data[i].attributes.user] + "\",\"rice\":\"" + jsonObj.data[i].attributes.rice + "\"}";
 			if(c != jsonObj.meta.pagination.total){
 				text += ",";
 			}
 			c++;
 		}
 	}
-	text += "]}";
+	text += "}}";
 	document.getElementById('jsontext').innerText = text;
 }
 loadData();
